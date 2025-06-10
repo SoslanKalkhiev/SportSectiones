afcab911afcaa0a1a5a4b3f69dd934c20bcea361
XmasSnorlax <skalhiev@gmail.com>
2025-06-10 11:15:07 +0300

    Добавьте файлы проекта.

---
+++ SportSectiones/Form1.cs
@@ -0,0 +1,10 @@
+namespace SportSectiones
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ SportSectiones/Section.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportSectiones
+{
+   
+    
+    internal class Section
+    {
+        public List<string> Students { get; set; }
+        public string Name { get; set; }
+        public string Coach { get; set; }
+        public int MaxStudents { get; set; }
+        public Section(string name, string coach, int maxStudents)
+        {
+            Students = new List<string>();
+            Name = name;
+            Coach = coach;
+            MaxStudents = maxStudents;
+           
+        }
+        public int FreeSpots
+        {
+            get { return MaxStudents - Students.Count; }
+        }
+        
+    }
+}
